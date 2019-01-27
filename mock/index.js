1dde3ad63866a8f3e72e1abb4a6078525b313c31
newbable <1558781725@qq.com>
2019-01-27 18:25:31 +0800

    feat:mock服务器搭建完成

--- mock/index.js
+++ mock/index.js
@@ -1,1 +1,17 @@
 //服务器搭建
+const express=require('express')
+const bodyParser=require('body-parser')
+const cors=require('cors')
+
+const app=express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(cors())
+// app.get('/test',(req,res)=>{
+//     res.json({
+//         name:'newbable'
+//     })
+// })
+app.listen(4995,()=>{
+    console.log('正在监听4995端口')
+})
